bbd14972d626d230079041b834ea970703fade1e
Patricio Capparelli <patriciocapparelli66@hotmail.com>
2024-06-06 23:57:55 -0300

    2.1

--- coder.js
+++ coder.js
@@ -1,84 +1,150 @@
+class Usuario {
+    constructor(nombreUsuario, apellidoUsuario, edadUsuario ){
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.edadUsuario = edadUsuario;
+    }
+    // metodo de la clase para validar el login
+    validar() {
+        const regexNombreApellido = /^[a-zA-Z]+$/;
+
+        if (typeof this.nombreUsuario === 'string' && typeof this.apellidoUsuario === 'string' && !isNaN(this.edadUsuario) && this.edadUsuario >= 18) {
+            if (regexNombreApellido.test(this.nombreUsuario) && regexNombreApellido.test(this.apellidoUsuario)) {
+                for (let property in this) {
+                    dataOfUser.push(this[property]); 
+                } // metodo de array dentro del (for...in) para iterar las propiedades de la clase y a su vez enviarlas al array. Unicamente si se cumple el condicional.
+                alert("Bienvenido " + this.nombreUsuario + " podes ingresar a tu cuenta :D");
+            } else {
+                alert('Ingrese un nombre y un apellido validos');
+            }
+        } else if (isNaN(this.edadUsuario)) {
+            alert('Ingrese una edad valida');
+        } else {
+            const añosFaltantes = 18 - this.edadUsuario;
+            alert('Te faltan ' + añosFaltantes + ' años para registrarte'); 
+        }
+    }
+}
+// guardando en constantes los datos del usuario.
 const nombreUsuario = prompt('Ingrese su nombre');
 const apellidoUsuario = prompt('Ingrese su apellido');
-const edadUsuario = Number(prompt('Ingresa tu edad'));
-// login del usuario
+const edadUsuario = parseInt(prompt('Ingrese su edad'));
 
-if (typeof nombreUsuario === 'string' && typeof apellidoUsuario === 'string' && !isNaN(edadUsuario) && edadUsuario >= 18) {
-        alert("Puedes ingresar a tu cuenta");
-    } else if (typeof nombreUsuario === 'number' || typeof apellidoUsuario === 'number') {
-        alert('Ingresa un nombre válido');
-    } else if (isNaN(edadUsuario)) {
-        alert('Ingresa un nombre y una edad válida');
-    } else {
-        const añosFaltantes = 18 - edadUsuario;
-        alert('Te faltan ' + añosFaltantes + ' años para registrarte');
-    }
+let usuario1 = new Usuario (nombreUsuario, apellidoUsuario, edadUsuario); // declarando una instancia con los datos
+let dataOfUser = []; 
+usuario1.validar(); // aplicandole el metodo a la instancia
+console.log(dataOfUser); // mostrando en consola el array con los datos del usuario
 
 // bucle 
+
+let carrito = [];
+
 let ropa;
-while (ropa != 0) {
-    let prenda = prompt('Ingrese la prenda que le interesa y le diremos su precio \n "remera" \n "pantalon" \n "zapatillas" \n "buzo" \n "gorra" \n para salir ingrese la palabra salir.');
-// opciones de ropa
-    switch(prenda) {
+while (ropa !== 'salir') {
+    ropa = prompt('Ingrese la prenda que le interesa y le diremos su precio \n "remera" \n "pantalon" \n "zapatillas" \n "buzo" \n "gorra" \n para salir ingrese la palabra salir.');
+    // opciones de ropa
+    let precioPrenda;
+    switch(ropa) {
         case 'remera':
-            alert('la remera cuesta 9000');
-        break;
+            precioPrenda = prompt('la remera cuesta $9000 ¿deseas comprarla? si/no');
+            if(precioPrenda.toLowerCase() === 'si'){
+                carrito.push(9000);
+            }
+            break;
         case 'pantalon':
-            alert('el pantalon cuesta 45000');
-        break;
+            precioPrenda = prompt('el pantalon cuesta $40000 ¿deseas comprarlo? si/no');
+            if(precioPrenda.toLowerCase() === 'si'){
+                carrito.push(40000);
+            }
+            break;
         case 'zapatillas':
-            alert('las zapatillas cuestan 189000');
-        break;
+            precioPrenda = prompt('las zapatillas cuestan $189000 ¿deseas comprarlas? si/no');
+            if(precioPrenda.toLowerCase() === 'si'){
+                carrito.push(189000);
+            }
+            break;
         case 'buzo':
-            alert('el buzo cuesta 83000');
-        break;
+            precioPrenda = prompt('el buzo cuesta $50000 ¿deseas comprarlo? si/no');
+            if(precioPrenda.toLowerCase() === 'si'){
+                carrito.push(50000);
+            }
+            break;
         case 'gorra':
-            alert('la gorra cuesta 12000');
-        break;
+            precioPrenda = prompt('la gorra cuesta $10000 ¿deseas comprarla? si/no');
+            if(precioPrenda.toLowerCase() === 'si'){
+                carrito.push(10000);
+            }
+            break;
         // salida del bucle
         case 'salir':
-            alert('esperamos que le haya gustado, si quiere puede consultar el precio final. Gracias!');
-            ropa = 0;
-        break;
+            alert('Buena eleccion!');
+            break;
         default:
-            alert("no se encuentra disponible esa prenda.");
-        continue; 
-        // volver al inicio del bucle si no es valido.
+            alert("No se encuentra disponible esa prenda.");
+            continue; // volver al inicio del bucle si no es valido.
     }
 }
+
+console.log(carrito);
+let totalProductos = carrito.length ; //cantidad de productos del (array) carrito asignada a una variable.
+let precioTotal = carrito.reduce((total, precio) => total + precio, 0); // metodo para sumar todos los productos del carrito.
+alert('Cantidad de productos: ' + totalProductos + '\nPrecio total: $' + precioTotal);
 
 // calcular precio final
-function calcularPrecioFinal(precioBase, impuestoPorcentaje, descuentoPorcentaje) {
-    if (isNaN(precioBase) || precioBase < 0) {
-        alert("precio inválido");
-        return; // Detener la ejecución 
+
+let calcIva = (value) => value * 0.21; // declarando una variable y asignandole una funcion flecha que calcula el iva.
+
+function realizarCompra(precioTotal) { // creando funcion que calcule el precio total.
+    let final = prompt('¿Desea realizar la compra? Le ofrecemos un descuento! Si/No');
+    if (final.toLowerCase() === 'si') {
+        function calcularPrecioFinal(precioBase, fn, descuentoPorcentaje) { // creando funcion para luego pasarle argumentos.
+            if (isNaN(precioBase) || precioBase < 0) {
+                alert("Precio inválido");
+                return; // Detener la ejecución 
+            }
+           // Descuento
+            const descuento = precioBase * (descuentoPorcentaje / 100);
+            const precioConDescuento = precioBase - descuento;
+
+            // Impuesto
+            const precioConIva = fn(precioConDescuento); //calculando precio con funcion por parametro y asignandolo a la constante.
+
+            // Precio final
+            const precioFinal = precioConDescuento + precioConIva;
+
+            // Return del precio final
+            return precioFinal;
+}
+        
+        let descuentoPorcentaje = 20; // variable de descuento con valor asignado
+
+        const precioFinalProducto = calcularPrecioFinal(precioTotal, calcIva, descuentoPorcentaje); // llamando a la funcion, pasandole los argumentos y asignando su return a la constante.
+
+        // Resultado final
+        if (precioFinalProducto !== undefined) {
+            // Mostrar solo si el precio final es valido
+            alert("Precio final del producto: " + precioFinalProducto + ' $');
+            alert("¡Vuelva pronto! :D");
+            console.log('su precio final por consola es: $'+ precioFinalProducto);
+            fechaDeCompra();
+        }
+    } else {
+        alert('Gracias por visitarnos, vuelva pronto!');
     }
-    // impuesto
-    const impuesto = precioBase * (impuestoPorcentaje / 100);
-
-    // descuento
-    const descuento = precioBase * (descuentoPorcentaje / 100);
-
-    // precio final
-    const precioFinal = precioBase + impuesto - descuento;
-
-    // return de precio final
-    return precioFinal;
 }
 
-// asignando un precio base a una variable
-let precioBaseProducto = Number(prompt('anote el precio del producto y le calculamos el precio final con descuento. ;)'));
-let impuestoPorcentaje = 10;
-let descuentoPorcentaje = 20;
-// utilizando la funcion para que se ejecute en torno a la variable que el usuario le asigna el precio base, calcula el precio final y lo guarda en otra nueva variable 
-const precioFinalProducto = calcularPrecioFinal(precioBaseProducto, impuestoPorcentaje, descuentoPorcentaje);
+// Llamando a la función para iniciar la compra pasando precioTotal como argumento.
 
-// resultado final
-if (precioFinalProducto !== undefined) {
-    // Mostrar solo si el precio final es valido
-    alert("precio final del producto: " + precioFinalProducto + ' $' );
-    alert("vuelva pronto! :D");
+realizarCompra(precioTotal);
+function fechaDeCompra() {
+    let tiempoActual = new Date(); //  llamando la clase 'Date' para ver la fecha y hora actual.
+    let fechaHoraFormateada = tiempoActual.toLocaleDateString('es-ES'); // utilizando el metodo para mostrar en español.
+    alert('Pago realizado el: ' + fechaHoraFormateada);
 }
+
+
+
+
 
 
 
